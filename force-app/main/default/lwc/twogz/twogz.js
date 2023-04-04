f519d0219a4ca58a8ebfd28ed30233959d21436d
Mbirepinte <mickael.birepinte@gmail.com>
2023-04-04 10:14:38 +0200

    get layout id ok

--- force-app/main/default/lwc/twogz/twogz.js
+++ force-app/main/default/lwc/twogz/twogz.js
@@ -1,7 +1,6 @@
 import { LightningElement, api, wire } from 'lwc';
 import { getRecord } from 'lightning/uiRecordApi';
 import ACCOUNT_NAME_FIELD from '@salesforce/schema/Account.Name';
-import ACCOUNT_RECORD_TYPE_ID_FIELD from '@salesforce/schema/Account.RecordTypeId';
 export default class Twogz extends LightningElement {
     error;
     data;
@@ -10,15 +9,24 @@ export default class Twogz extends LightningElement {
     hour;
     record;
     object;
+    objectApiName;
+    layout;
+    
+    handleLoad(event) {
+        console.log("detail", event.detail);
+        console.log('Layout => ', event.detail.layouts);
+        console.log('test',Object.keys(event.detail.layoutUserStates)[0]);
+        this.layout=Object.keys(event.detail.layoutUserStates)[0];
+    };
 
     @api recordId;
-
     @api objectApiName;
     @wire(getRecord, { recordId: '$recordId', fields: [ACCOUNT_NAME_FIELD] })
     wiredRecord({ error, data }) {
         console.log('data', data);
         if (data) {
             this.data = data;
+            this.fields= data.fields.name;
             this.error = undefined;
             this.date=data.lastModifiedDate;
             this.id=data.id;
@@ -28,5 +36,5 @@ export default class Twogz extends LightningElement {
             this.data = undefined;
         }
     }
- 
+    
 }
